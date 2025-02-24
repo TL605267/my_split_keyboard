8198f9cba0708343d699ae23b3a6362e72820d21
tl605267 <lvtongtom@gmail.com>
2025-02-23 21:22:00 -0600

    Found bug

--- hardware/choc_v1_socket_reversible_165_595/paste2cut.py
+++ hardware/choc_v1_socket_reversible_165_595/paste2cut.py
@@ -3,14 +3,14 @@ from os import listdir
 from enum import Enum, auto
 
 class State(Enum):
-	INIT = auto()
 	COPY = auto()
 	SKIP = auto()
+	MOD = auto()
 
 class gen_vinyl_cut():
 	def __init__(self):
 		self.paste_svg = [file for file in listdir('.') if file.endswith('B_Paste.svg')][0]
-		self.state = State.INIT
+		self.state = State.COPY
 		self.style_template = '''<style>
 	.outlineStyle {
 	  fill:none; 
@@ -27,24 +27,34 @@ class gen_vinyl_cut():
 	def process_svg(self):
 		with open(self.paste_svg, 'r') as paste_file, open('autogen_cut.svg', 'w') as output:
 			for line in paste_file.readlines():
-				# TODO: change init to copy and add modify
-				if self.state == State.INIT:
+				# state transition
+				if self.state == State.COPY:
 					if line.startswith('<title'):
-						output.write(self.style_template)
-						self.state = State.COPY
-					output.write(line)                             
-				elif self.state == State.COPY:
-					if line.startswith('<path style'):
+							self.state = State.MOD
+					elif line.startswith('<path style'):
 						if 'fill:none;' in line:
 							self.state = State.SKIP
 						else:
-							output.write(self.path_style)
-					else: 
-						output.write(line)
+							self.state = State.MOD
 				elif self.state == State.SKIP:
-					if '/>' in line:
+					if '/>' in line: 
+						# FIXME move to COPY in next state
 						self.state = State.COPY
-
+				elif self.state == State.MOD:
+					self.state = State.COPY
+				# state action
+				if self.state == State.COPY:
+					output.write(line)
+				elif self.state == State.SKIP:
+					continue
+				elif self.state == State.MOD:
+					if line.startswith('<title'):
+						output.write(self.style_template)
+						output.write(line)
+					elif line.startswith('<path style'):
+						output.write(self.path_style)
+						
+					
 def main():
 	m = gen_vinyl_cut()                                              
 	m.process_svg()                                          
