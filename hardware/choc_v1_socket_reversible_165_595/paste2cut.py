701e068fdd2ea6bc6bba8ba36e7b1098c92d1959
tl605267 <lvtongtom@gmail.com>
2025-02-23 23:57:35 -0600

    Replace group style instead of adding style class

--- hardware/choc_v1_socket_reversible_165_595/paste2cut.py
+++ hardware/choc_v1_socket_reversible_165_595/paste2cut.py
@@ -6,46 +6,44 @@ class State(Enum):
 	COPY = auto()
 	SKIP = auto()
 
-class gen_vinyl_cut():
+class GenVinylCut:
 	def __init__(self):
-		# TODO select F/B_Paste svg file
-		self.paste_svg = [file for file in listdir('.') if file.endswith('F_Paste.svg')][0]
+		# Select F/B_Paste svg file
+		try:
+			self.paste_svg = next(file for file in listdir('.') if file.endswith('F_Paste.svg'))
+		except StopIteration:
+			raise FileNotFoundError("No F_Paste.svg file found in the current directory.")
+		
 		self.state = State.COPY
-		self.style_template = '''<style>
-	.outlineStyle {
-	  fill:none; 
-	  stroke:#000000; 
-	  stroke-width:0.0500; 
-	  stroke-opacity:1; 
-	  stroke-linecap:round;  
-	  stroke-linejoin:round;
-	}
-</style>
-'''
-		self.path_style = '<path class="outlineStyle"\n'
+		# gray stroke for easier look in dark mode :)
+		self.style_template ='''<g style="fill:none; stroke:#999999; stroke-width:0.0500; stroke-opacity:1; '''
+		self.path_style = '<path '
 
 	def process_svg(self):
-		with open(self.paste_svg, 'r') as paste_file, open('autogen_cut.svg', 'w') as output:
-			for line in paste_file.readlines():
-				if self.state == State.COPY:
-					if line.startswith('<title'): # add style defnition before title
-						output.write(self.style_template)
-						output.write(line)                             
-					elif line.startswith('<path style'):
-						if 'fill:none;' in line: # existing edge cut paths
-							self.state = State.SKIP
-						else: # pads that needs to be converted to stroke
-							output.write(self.path_style)
-					else: 
-						output.write(line)
-
-				elif self.state == State.SKIP: 
-					if '/>' in line: # skip <path fill:none .../> block
-						self.state = State.COPY
+		try:
+			with open(self.paste_svg, 'r') as paste_file, open('autogen_cut.svg', 'w') as output:
+				for line in paste_file:
+					if self.state == State.COPY:
+						if line.startswith('<g'):  # Replace group style
+							output.write(self.style_template)
+						elif line.startswith('<path style'):
+							if 'fill:none;' in line:  # Existing edge cut paths
+								self.state = State.SKIP
+							else:  # Pads that need to be converted to stroke
+								output.write(self.path_style)
+						else:
+							output.write(line)
+					elif self.state == State.SKIP:
+						if '/>' in line:  # Skip <path fill:none .../> block
+							self.state = State.COPY
+		except FileNotFoundError as e:
+			print(f"Error: {e}")
+		except IOError as e:
+			print(f"Error: {e}")
 
 def main():
-	m = gen_vinyl_cut()                                              
-	m.process_svg()                                          
-	                                          
-if __name__ == "__main__":                                 
+	m = GenVinylCut()
+	m.process_svg()
+
+if __name__ == "__main__":
 	main()
